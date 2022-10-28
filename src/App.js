a942c77f5524d88ec3baf6404a3d96b07eabb207
YaraPolk <yaroslav260493@gmail.com>
2022-10-28 11:00:23 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -10,25 +10,41 @@ const initNotes = [
     {
         id: id(),
         name: 'name1',
-        desc: 'long description 1'
+        desc: 'long description 1',
+        show: false,
     },
     {
         id: id(),
         name: 'name2',
-        desc: 'long description 2'
+        desc: 'long description 2',
+        show: false,
     },
     {
         id: id(),
         name: 'name3',
-        desc: 'long description 3'
+        desc: 'long description 3',
+        show: false,
     },
 ];
 
 function App() {
     const [notes, setNotes] = useState(initNotes);
 
+    function showDesc(id) {
+        setNotes(notes.map(note => {
+            if (note.id !== id) {
+                return note;
+            } else {
+                return {...note, show: !note.show};
+            }
+        }))
+    }
+
     const result = notes.map(note => {
-        return <p key={note.id}>{note.name}, {note.desc}</p>;
+        return <p key={note.id}>
+            {note.name}, {note.show && note.desc}
+            <button onClick={() => showDesc(note.id)}>{note.show ? "hide" : "show"}</button>
+        </p>;
     })
 
     return (
