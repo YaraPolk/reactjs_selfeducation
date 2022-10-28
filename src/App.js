2eaa7cce3ba655c64113495dfd0416871a7cf5d3
YaraPolk <yaroslav260493@gmail.com>
2022-10-28 12:29:09 +0300

    Редактирование тега появляющимся инпутом в React

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
 import './App.css';
 import {useState} from "react";
-import uuid from "react-uuid";
-
-function id() {
-    return uuid();
-}
-
-const initNotes = [
-    {
-        id: id(),
-        name: 'name1',
-        desc: 'long description 1',
-        show: false,
-    },
-    {
-        id: id(),
-        name: 'name2',
-        desc: 'long description 2',
-        show: false,
-    },
-    {
-        id: id(),
-        name: 'name3',
-        desc: 'long description 3',
-        show: false,
-    },
-];
 
 function App() {
-    const [notes, setNotes] = useState(initNotes);
+    const [value, setValue] = useState('hello someone');
+    const [isEdit, setIsEdit] = useState(false);
 
-    function showDesc(id) {
-        setNotes(notes.map(note => {
-            if (note.id !== id) {
-                return note;
-            } else {
-                return {...note, show: !note.show};
-            }
-        }))
+    let elem = !isEdit ? <p>{value}</p> : <input type="text" value={value} onChange={event => setValue(event.target.value)} />;
+
+    function change(event){
+        if (event.target.innerText === 'edit'){
+            setIsEdit(true);
+        } else {
+            setIsEdit(false);
+        }
     }
-
-    const result = notes.map(note => {
-        return <p key={note.id}>
-            {note.name}, {note.show && note.desc}
-            <button onClick={() => showDesc(note.id)}>{note.show ? "hide" : "show"}</button>
-        </p>;
-    })
 
     return (
         <>
-            {result}
+            {elem}
+            <button onClick={event => change(event)}>edit</button>
+            <button onClick={event => change(event)}>show</button>
         </>
     );
 }
